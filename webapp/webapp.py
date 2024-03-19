5ac81855b4943e2540182381d9f255eb383f8f55
Karanvir93875 <ksin3798@gmail.com>
2024-03-19 09:55:46 -0400

    connectting to mysql db

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,10 +1,26 @@
+import mysql.connector
 from flask import Flask
 
 app = Flask(__name__)
 
+# Database connection parameters
+db_config = {
+    'host': 'your-rds-host.amazonaws.com',
+    'user': 'your-master-username',
+    'password': 'your-password',
+    'database': 'your-database-name'
+}
+
+# Establishing a connection to the MySQL database
+try:
+    conn = mysql.connector.connect(**db_config)
+    print("Connection to MySQL database successful")
+except mysql.connector.Error as e:
+    print(f"Error connecting to MySQL: {e}")
+
 @app.route('/')
 def home():
-    return "Welcome to The Virtual Pet Adoption Platform"
+    return "Welcome to PetPal Virtual Adoption Platform!"
 
 if __name__ == '__main__':
     app.run(debug=True)
